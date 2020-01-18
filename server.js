bce231d4c4d14982e8a292ce0c04b50f81cfb7ab
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-01-18 17:54:29 -0600

    initialize port and test index file on server.js and add requires for express and handlebars to that

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require("express");
+const exphbs = require ("express-handlebars");
+
+app = express ();
+const PORT = process.env.PORT || 2020;
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({defaultLayout:"main"}));
+app.set("view engine", "handlebars");
+
+
+app.get("/", function (err,res) {
+
+res.render("index", res);
+
+ });
+
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+})
