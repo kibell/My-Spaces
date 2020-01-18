1f1de90465e45dd07956b38f20804ce76c2798d0
Kitrick Bell <kitrickd.bell@gmail.com>
2020-01-18 01:20:55 -0600

    connected to the port and installed packages

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+require("./routes/html-routes.js")(app);
+
+
+
+app.listen(PORT, function() {
+  console.log("Listening on port", PORT);
+});
+
+
+
+
+
+
+//login authenthication
