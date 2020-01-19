ef82e3e502e05e489b48f0c2e8f7401f628f53d8
Kitrick Bell <kitrickd.bell@gmail.com>
2020-01-19 14:56:44 -0600

    Fixing authentication bugs

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,18 +1,99 @@
 // =============================================================
 const path = require("path");
 
-
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function(app, passport) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
+  
+
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.render('index.html')
   });
 
 
 
-};
+  app.get("/dashboard", isLoggedIn,  function(req, res, next) {
+      console.log("logged in!!!")
+    res.send("../public/dashboard.html")
+  });
+
+
+  app.get("/register", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/register.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+
+
+
+  app.post('/register', passport.authenticate('local-register', {
+    successRedirect: '/dashboard',
+
+    failureRedirect: '/register'
+}
+
+
+
+
+
+));
+
+app.post('/login', passport.authenticate('local-login', {
+   
+   
+    successRedirect: '/dashboard',
+
+    failureRedirect: '/login',
+
+    failureFlash: true 
+}
+
+));
+
+
+
+app.get('/logout',function(req, res){
+   
+ 
+        req.session.destroy(function(err) {
+     
+            res.redirect('/');
+     
+        });
+     
+
+
+})
+
+
+
+
+
+
+
+
+function isLoggedIn(req, res, next) {
+ 
+    if (req.authenticated){
+
+        return next();
+    }
+
+    res.redirect('/login');
+
+}
+
+
+
+}
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require("express");
 
+const express = require('express');
 const PORT = process.env.PORT || 8000;
 const app = express();
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const env = require('dotenv')
+const exphbs = require('express-handlebars')
+//Models
+const models = require("./models")
+
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -10,13 +19,38 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const exphbs = require("express-handlebars");
+
+//for PassPort
+
+require('./config/passport/passport')(passport,models.user)
+app.use(session({ secret: 'keyboard cat',
+resave: true, 
+saveUninitialized:true})); // session secret
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+
+
+//For handlebars
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-require("./routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app,passport);
 
+
+
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice!')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
+});
 
 
 app.listen(PORT, function() {
@@ -27,5 +61,3 @@ app.listen(PORT, function() {
 
 
 
-
-//login authenthication
