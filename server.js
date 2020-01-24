4ebcdf36aa793dff8a694f4723abbb0a505fd7f0
Kitrick Bell <kitrickd.bell@gmail.com>
2020-01-23 18:43:54 -0600

    merging master

--- server.js
+++ server.js
@@ -26,8 +26,6 @@ const models = require("./models")
 
 
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
 
 // Parse application body
 app.use(express.urlencoded({ extended: true }));
