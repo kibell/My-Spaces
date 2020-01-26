45782dfa6dc580ae862e2a8027794ab14042738b
Dipti Katkar <dk.aj1990@gmail.com>
2020-01-26 07:39:33 -0600

    Updating storage controller file display storage data

--- public/assets/js/storage.js
+++ public/assets/js/storage.js
@@ -16,16 +16,16 @@ $(document).ready(function() {
     // Getting the initial list of storages
     getStorages();
   
-    // A function to handle what happens when the form is submitted to create a new Area
-    function handleAreaFormSubmit(event) {
+    // A function to handle what happens when the form is submitted to create a new Storage
+    function handleStorageFormSubmit(event) {
       event.preventDefault();
       // Don't do anything if the name fields hasn't been filled out
-      if (!nameInput.val().trim().trim()) {
+      if (!storageInput.val().trim().trim()) {
         return;
       }
-      // Calling the upsertArea function and passing in the value of the name input
-      upsertArea({
-        name: nameInput
+      // Calling the upsertStorage function and passing in the value of the name input
+      upsertStorage({
+        name: storageInput
           .val()
           .trim()
       });
@@ -43,23 +43,23 @@ $(document).ready(function() {
   //     }
   // }
   
-    // A function for creating an author. Calls getareas upon completion
-    function upsertArea(areaData) {
-      $.post("/api/areas", areaData)
-        .then(getareas);
+    // A function for creating a storage. Calls getStorages function upon completion
+    function upsertStorage(storageData) {
+      $.post("/api/storages", storageData)
+        .then(getStorages);
     }
   
-    // Function for creating a new list row for areas
-    function createAreaRow(areaData) {
+    // Function for creating a new list row for storage
+    function createAreaRow(storageData) {
       const newTr = $("<div>");
-      newTr.attr("data-area", areaData);
-      newTr.attr("data-target", "#new-storages");
-      newTr.attr("data-toggle", "modal");
+      newTr.attr("data-area", storageData);
+     // newTr.attr("data-target", "#new-storages");
+      //newTr.attr("data-toggle", "modal");
       newTr.append("<div>" + areaData.name + "</div>");
       newTr.addClass("new-storage");
       newTr.addClass("btn");
       
-      if (areaData.user) {
+      if (storageData.user) {
         // newTr.append("<div> " + areaData.user.id + "</div>");
       } else {
         newTr.append("<td>0</td>");
@@ -70,51 +70,51 @@ $(document).ready(function() {
     }  
 
   
-    // Function for retrieving areas and getting them ready to be rendered to the page
-    function getareas() {
-      $.get("/api/areas", function(data) {
+    // Function for retrieving storages and getting them ready to be rendered to the page
+    function getStorages() {
+      $.get("/api/storages", function(data) {
         const rowsToAdd = [];
         for (let i = 0; i < data.length; i++) {
-          rowsToAdd.push(createAreaRow(data[i]));
+          rowsToAdd.push(createStorageRow(data[i]));
         }
-        renderAreaList(rowsToAdd);
-        nameInput.val("");
+        renderStorageList(rowsToAdd);
+        storageInput.val("");
       });
     }
   
     // A function for rendering the list of areas to the page
-    function renderAreaList(rows) {
+    function renderStorageList(rows) {
       // areaList.children().not(":last").remove();
-      areaContainer.children(".alert").remove();
+      storageContainer.children(".alert").remove();
       if (rows.length) {
         console.log(rows);
-        areaList.prepend(rows);
+        storageList.prepend(rows);
       }
       else {
         renderEmpty();
       }
     }
   
-    // Function for handling what to render when there are no areas
+    // Function for handling what to render when there are no Storages
     function renderEmpty() {
       const alertDiv = $("<div>");
       alertDiv.addClass("alert alert-danger");
-      alertDiv.text("Create an Area By Clicking the + sign .")
-      areaContainer.append(alertDiv);
+      alertDiv.text("Create a new Storage By Clicking the + sign .")
+      storageContainer.append(alertDiv);
     }
   
     // Function for handling what happens when the delete button is pressed
     function handleDeleteButtonPress() {
-      console.log(`Delete area button pressed`); 
+      console.log(`Delete storage button pressed`); 
       const listItemData = $(this).parent("td").parent("tr").data("area");
       console.log(`List item data for delete area ${listItemData}`); 
       const id = listItemData.id;
       console.log(id); 
       $.ajax({
         method: "DELETE",
-        url: "/api/areas/" + id
+        url: "/api/storages/" + id
       })
-        .then(getareas);
+        .then(getStorages);
     }
 
     $(document).on("click", ".new-storage", function () {
@@ -122,6 +122,6 @@ $(document).ready(function() {
       // $('#new-storages').on('shown.bs.modal', function () {
       //   $('#myInput').trigger('focus')
       // })
-      createAreaRow()
+      createStorageRow()
     })
   });
