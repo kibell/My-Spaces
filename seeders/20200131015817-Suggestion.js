f2ae930a86fc1cad713ffe7f23e6f3e2c8eb8a92
Dipti Katkar <dk.aj1990@gmail.com>
2020-01-30 20:01:30 -0600

    fix merge conflicts seeders

--- seeders/20200131015817-Suggestion.js
+++ seeders/20200131015817-Suggestion.js
@@ -1,32 +1,7 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.bulkInsert(
-    'Suggestions',
-    [
-      {
-        name: 'First aid kit', 
-        storage_type: 'General Storage', 
-        description: 'First aid kit'
-      }, 
-      {
-        name: 'Batteries', 
-        storage_type: 'General Storage', 
-        description: 'Batteries'
-      }, 
-      {
-        name: 'Light bulbs', 
-        storage_type: 'General Storage', 
-        description: ''
-      },
-      {
-        name: 'Trash bags', 
-        storage_type: 'General Storage', 
-        description: ''
-      }
-
-    ],
-    {
+  up: (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -37,9 +12,9 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
-  }),
+  },
 
-  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Suggestions', null, {}),
+  down: (queryInterface, Sequelize) => {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
@@ -47,5 +22,5 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
-  
+  }
 };
